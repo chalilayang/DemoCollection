77995914bcd75439ed2fe519996a1759a04e61d7
chalilayang <chalilayang@sohu-inc.com>
2016-04-28 15:31:06 +0800

    update

--- app/src/main/java/com/chalilayang/test/CustomViewActivity.java
+++ app/src/main/java/com/chalilayang/test/CustomViewActivity.java
@@ -1,29 +1,20 @@
 package com.chalilayang.test;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.Region;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Display;
-import android.view.View;
 
 import com.chalilayang.test.customview.BitmapMeshView;
-import com.chalilayang.test.customview.MeshView;
-import com.chalilayang.test.customview.PlasmaView;
+import com.chalilayang.test.customview.WaveTestView;
 
 public class CustomViewActivity extends Activity {
     /* load our native library */
     static {
-        System.loadLibrary("plasma");
+//        System.loadLibrary("plasma");
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Display display = getWindowManager().getDefaultDisplay();
         setContentView(R.layout.activity_customview_layout);
     }
 }
