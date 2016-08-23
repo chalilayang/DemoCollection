aea9d18718ac87640df11bab49bc653f694230a1
U-SOHU-INC\chalilayang <chalilayang@CHALILAYANG2-TJ.sohu-inc.com>
2016-08-23 16:53:16 +0800

    update some stylesV21 about status_bar

--- app/src/main/java/com/chalilayang/test/ScrollingActivity.java
+++ app/src/main/java/com/chalilayang/test/ScrollingActivity.java
@@ -1,5 +1,6 @@
 package com.chalilayang.test;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -8,8 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
-
-import com.chalilayang.test.R;
+import android.view.WindowManager;
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -19,6 +19,16 @@ public class ScrollingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        requestWindowFeature(Window.);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            Window window = getWindow();
+//            // Translucent status bar
+//            window.setFlags(
+//                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+//                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//        }
+//        int newVis = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+//        getWindow().getDecorView().setSystemUiVisibility(newVis);
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
